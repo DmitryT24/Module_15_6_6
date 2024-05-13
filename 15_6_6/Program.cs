8e0dffff1bc4e1ff0252de5b57a5ef5d8df5f24d
DmitryT24 <t.tdn@yandex.ru>
2024-05-13 22:57:31 +0300

    Добавьте файлы проекта.

---
+++ 15_6_6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            //1) 
+
+            return classes.SelectMany(x => x.Students).ToArray();
+
+            //2
+            //return (from s in classes
+            //        from a in s.Students
+            //        select a).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
+
+//??? - подробно еще раз посмотреть разницу - ???
+//https://learn.microsoft.com/ru-ru/dotnet/csharp/linq/standard-query-operators/projection-operations#select-versus-selectmany
+
